9e9d972c65da5942e1c34b02a9497d4b2005737a
AaravNair04 <nairaarav17@gmail.com>
2025-08-13 12:28:18 +0000

    problem 4

---
+++ area_rect.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+	int length = 10, breadth = 6;			// length and width of rectangle
+	std::cout << "Area of rectangle with length '" << length << "' and width '" << breadth << "' is: " << length*breadth;				// area of rect = l*b
+}
