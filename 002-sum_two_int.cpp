e96d6ab75410e585b6875170959af3ca12fa2b53
Aarav Nair <nairaarav17@gmail.com>
2025-08-15 13:45:47 +0530

    13-08-2025

--- 002-sum_two_int.cpp
+++ 002-sum_two_int.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main(){
 	int num1, num2;
-	std::cout << "Enter first number: ";		// Asks user for first number
-	std::cin >> num1;				// Assigns input to num1
-	std::cout << "Enter second number: ";	// Asks user for second number
-	std::cin >> num2;				// Assigns input to num2
-	std::cout << "Sum of " << num1 <<" and "<< num2 <<" is " << (num1+num2);        // output
+	cout << "Enter first number: ";		// Asks user for first number
+	cin >> num1;				// Assigns input to num1
+	cout << "Enter second number: ";	// Asks user for second number
+	cin >> num2;				// Assigns input to num2
+	cout << "Sum of " << num1 <<" and "<< num2 <<" is " << (num1+num2);        // output
 }
