ec896b5d0090bb8319111f3df550be02563e2bad
Aarav Nair <nairaarav17@gmail.com>
2025-08-15 13:49:11 +0530

    14-08-2025

--- 006-simple_interest_calc.cpp
+++ 006-simple_interest_calc.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main(){
 	int principal, rate, time;
-	std::cout << "Enter initial Principal: ";
-	std::cin >> principal;
-	std::cout << "Enter annual interest rate: ";
-	std::cin >> rate;
-	std::cout << "Enter time(in years): ";
-	std::cin >> time;
+	cout << "Enter initial Principal: ";
+	cin >> principal;
+	cout << "Enter annual interest rate: ";
+	cin >> rate;
+	cout << "Enter time(in years): ";
+	cin >> time;
 	int simple_interest = (principal*rate*time)/100;
-	std::cout << "Simple interest earned on a principal of " << principal << " rupees with an annual interest rate of " << rate << " percent after a time period of " << time << " years is: " <<simple_interest;
+	cout << "Simple interest earned on a principal of " << principal << " rupees with an annual interest rate of " << rate << " percent after a time period of " << time << " years is: " <<simple_interest;
+	return 0;
 } 
