09c22ed04d4435fd3b25b4cd98f8944e64775b98
AaravNair04 <nairaarav17@gmail.com>
2025-08-15 08:03:41 +0000

    15-08-25

---
+++ 012-largest_two_nums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int num1, num2;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	
+	if (num1>num2)
+		cout << num1 << " is the greatest number"; 
+	else if (num2>num1)
+		cout << num2 << " is the greatest number";
+	else								// if num1 == num2
+		cout << "Both numbers are equal";
+	
+	return 0;
+}
