916ef90d044a3df9cf5ebcd78a69558e9e2e0134
Aarav Nair <nairaarav17@gmail.com>
2025-08-15 13:46:29 +0530

    13-08-2025

--- 003-hello_name.cpp
+++ 003-hello_name.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 int main(){
-	std::string name;				// declare string variable
-	std::cout << "Enter your name: ";		// input from user
-	std::cin >> name;				// store input in string variable
-	std::cout << "Hello, " << name << "!"; 		// output
+	string name;				// declare string variable
+	cout << "Enter your name: ";		// input from user
+	cin >> name;				// store input in string variable
+	cout << "Hello, " << name << "!"; 		// output
 	return 0;
 }
